a7b132427a0deb339fee68d7c4d5a326ffe02dc7
Dmitry Khmelyk <delinger360@gmail.com>
2022-06-25 19:18:27 +0300

    updated styling and provided routing

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Header.css'
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const goToLots = () => navigate("/lots");
+
   return (
     <header className='header'>
       <h3 className='logo-text'>Trade<span className='colored'>Me</span></h3>
       <div className='nav-container'>
-      <button className='login-button'>Sign In</button>
-      <h3 className='nav-element'>Lots</h3>
-      <h3 className='nav-element'>Other</h3>
+      <button className='login-button'>SIGN IN</button>
+      <h3 className='nav-element' onClick={goToLots}>LOTS</h3>
+      <h3 className='nav-element'>OTHER</h3>
       </div>
     </header>
   )
--- src/components/pages/Landing/Landing.jsx
+++ src/components/pages/Landing/Landing.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Landing.css'
 
 const Landing = () => {
+  const navigate = useNavigate();
+
+  const goToLots = () => navigate("/lots");
+
   return (
     <section className='banner-section'>
-      <h1 class="banner">HOW MANY LOTS DO WE GOT?</h1>
+      <h1 class="banner">WHAT DO WE GOT AND HOW MANY?</h1>
       <h2 className='banner colored'>A LOT!</h2>
       <p className='notion'>With TradeMe you can put on auction <span className='colored'>almost</span> anything</p>
-      <button className='search-button'>Start searching</button>
+      <button className='search-button' onClick={goToLots}>Start searching</button>
     </section>
   )
 }
